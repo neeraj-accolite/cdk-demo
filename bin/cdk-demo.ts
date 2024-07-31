1ec7a4125098e6a1cfa41c0595bbf3821fba4456
Neeraj Agrawal <neeraj.agrawal@accolitedigital.com>
2024-07-31 18:43:14 +0530

    added env variable

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -5,5 +5,10 @@ import { CDKDemoPiplelineStack } from '../lib/cdk-demo-pipeline-stack';
 
 const app = new cdk.App();
 // new CdkDemoStack(app, 'CdkDemoStack');
-new CDKDemoPiplelineStack(app, 'CdkDemoPipelineStack');
+new CDKDemoPiplelineStack(app, 'CdkDemoPipelineStack', {
+    env: {
+        account: process.env.ACCOUNT_ID,
+        region: process.env.REGION_NAME
+    }
+});
 
