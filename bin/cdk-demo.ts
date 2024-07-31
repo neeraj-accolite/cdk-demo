fe8a4fe68255b0dd81e07901bb9f2aee93a1fe04
Neeraj Agrawal <neeraj.agrawal@accolitedigital.com>
2024-07-31 20:33:18 +0530

    removed env

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-// import { CdkDemoStack } from '../lib/cdk-demo-stack';
-import { CDKDemoPiplelineStack } from '../lib/cdk-demo-pipeline-stack';
+import { CdkDemoStack } from '../lib/cdk-demo-stack';
+// import { CDKDemoPiplelineStack } from '../lib/cdk-demo-pipeline-stack';
 
 const app = new cdk.App();
-// new CdkDemoStack(app, 'CdkDemoStack');
-console.log('accout 590376488337 ', process.env.account);
-console.log('region ', process.env.region);
-new CDKDemoPiplelineStack(app, 'CdkDemoPipelineStack', {
+new CdkDemoStack(app, 'CdkDemoStack', {
     env: {
-        account: '590376488337',
-        region: 'us-east-1'
+        account: process.env.account,
+        region: process.env.region
     }
 });
+// new CDKDemoPiplelineStack(app, 'CdkDemoPipelineStack', {
+//     env: {
+//         account: '590376488337',
+//         region: 'us-east-1'
+//     }
+// });
 
